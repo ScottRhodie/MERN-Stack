d1adba1d3d212307a14ce9f3d245f0513028f1aa
ScottRhodie <scott.rhodie@guidesmiths.com>
2019-02-21 11:56:41 +0100

    added profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -56,7 +56,63 @@ const ProfileSchema = new Schema({
         },
         current: {
             type: Boolean,
-            default: true
+            default: false
         },
-    }]
-})
+        description: {
+            type: String
+        }
+    }],
+    Education: [{
+        school: {
+            type: String,
+            required: true
+        },
+        degree: {
+            type: Strong,
+            required: true
+        },
+        fieldofstudy: {
+            type: String,
+            required
+        },
+        from: {
+            type: Date,
+            required: true
+        },
+        to: {
+            type: Date
+        },
+        current: {
+            type: Boolean,
+            default: false
+        },
+        description: {
+            type: String
+        }
+    }],
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+
+    },
+    date: {
+        type: Date,
+        default: Date.now
+
+    }
+});
+
+modeule.exports = Profile = mongoose.model("profile", ProfileSchema)
